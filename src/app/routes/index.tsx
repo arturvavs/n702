4afca239cc6aadb7178b2af95077c19b3fdaa158
Artur Araujo <arturavs1996@gmail.com>
2024-10-29 22:48:01 -0300

    inclusão da página agendamento com um datepicker

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -3,6 +3,7 @@ import { Login } from "../pages/login/Login"
 import { CadastroUsuario } from "../pages/cadastro/CadastroUsuario"
 import { UserPager } from "../pages/user_page/UserPage"
 import PrivateRoute from "../components/PrivateRoute"
+import { Agendamento } from "../pages/user_page/Agendamento"
 
 export const Routes = () => {
     return (
@@ -11,6 +12,7 @@ export const Routes = () => {
                 <Route path ="/" element={<Login />}/>
                 <Route path ="/gerenciamento" element={<PrivateRoute><UserPager /></PrivateRoute>}/>
                 <Route path ="/cadastroUsuario" element={<CadastroUsuario />}/>
+                <Route path ="/agendamento" element={<Agendamento />} />
             </Switch>
         </BrowserRouter>
     )
